ae3546f917c9bff4d342be84ec14bbb6a6a6305a
Kent <asilo.kentbryanv19@gmail.com>
2024-10-25 21:04:37 +0800

    DISPATCH MONITORING DONE

--- src/app/dispatch-monitoring/page.tsx
+++ src/app/dispatch-monitoring/page.tsx
@@ -6,6 +6,8 @@ import { FaBus, FaCog, FaUsers, FaAngleDoubleRight } from "react-icons/fa";
 
 const DispatchMonitoring = () => {
   const [dropdownVisible, setDropdownVisible] = useState(false);
+  const [selectedBus, setSelectedBus] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1); // Page state
   const dropdownRef = useRef(null);
 
   // Sample bus data simulating a backend response
@@ -16,7 +18,29 @@ const DispatchMonitoring = () => {
     { number: "004", status: "On Road" },
     { number: "005", status: "On Road" },
     { number: "006", status: "On Alley" },
+    { number: "007", status: "On Road" },
+    { number: "008", status: "Maintenance" },
+    { number: "009", status: "On Alley" },
+    { number: "010", status: "Maintenance" },
+    { number: "011", status: "On Road" },
+    { number: "012", status: "On Road" },
   ];
+
+  const itemsPerPage = 6;
+  const totalPages = Math.ceil(busData.length / itemsPerPage);
+
+  // Handle page change
+  const handlePageChange = (page) => {
+    if (page >= 1 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
+
+  // Filter bus data for the current page
+  const displayedBuses = busData.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
 
   // Close dropdown if clicking outside of it
   useEffect(() => {
@@ -35,9 +59,9 @@ const DispatchMonitoring = () => {
       <Sidebar />
       <div className="w-full bg-slate-200">
         <Header title="Dispatch Monitoring" />
-        <section className="right w-full  overflow-y-hidden">
+        <section className="right w-full overflow-y-hidden">
           <div className="content flex flex-col h-full ml-20">
-            <div className="bus-location ">
+            <div className="bus-location">
               <div className="output flex flex-row space-x-2 mt-8">
                 <div className="locations w-auto bg-white h-auto rounded-lg border-2 border-violet-400">
                   <iframe
@@ -54,9 +78,8 @@ const DispatchMonitoring = () => {
             </div>
             <div className="bus-info flex flex-row mt-12 space-x-8">
               <div className="col-bus space-y-4">
-                {/* Loop through the first set of bus numbers */}
-                {busData.slice(0, 3).map((bus) => {
-                  // Assign background color based on the status
+                {/* Loop through displayed buses for the current page */}
+                {displayedBuses.slice(0, 3).map((bus) => {
                   const bgColor =
                     bus.status === "Maintenance"
                       ? "bg-slate-400"
@@ -64,44 +87,40 @@ const DispatchMonitoring = () => {
                       ? "bg-yellow-400"
                       : "bg-blue-400";
 
-                  // Assign text color for the status (red if Maintenance)
                   const textColor =
                     bus.status === "Maintenance"
                       ? "text-red-500"
                       : "text-black";
+                  const isSelected = selectedBus === bus.number;
+                  const selectedBorder = isSelected
+                    ? "border-4 border-green-500"
+                    : "";
 
                   return (
-                    <div
+                    <button
                       key={bus.number}
-                      className={`container w-96 p-4 rounded-lg flex flex-row space-x-14 ${bgColor}`}
+                      onClick={() => setSelectedBus(bus.number)}
+                      className={`container w-96 p-4 rounded-lg flex flex-row space-x-14 ${bgColor} ${selectedBorder}`}
                     >
-                      {/* Icon */}
                       <FaBus
                         size={30}
                         className="ml-2 cursor-pointer text-black"
                       />
-
-                      {/* Bus number */}
                       <h1>
                         Bus{" "}
                         <span id={`bus-number-${bus.number}`}>
                           {bus.number}
                         </span>
                       </h1>
-
-                      {/* Bus status with dynamic text color */}
                       <h1 id={`status-${bus.number}`} className={textColor}>
                         {bus.status}
                       </h1>
-                    </div>
+                    </button>
                   );
                 })}
               </div>
-
               <div className="col-bus space-y-4">
-                {/* Loop through the second set of bus numbers */}
-                {busData.slice(3).map((bus) => {
-                  // Assign background color based on the status
+                {displayedBuses.slice(3).map((bus) => {
                   const bgColor =
                     bus.status === "Maintenance"
                       ? "bg-slate-400"
@@ -109,38 +128,84 @@ const DispatchMonitoring = () => {
                       ? "bg-yellow-400"
                       : "bg-blue-400";
 
-                  // Assign text color for the status (red if Maintenance)
                   const textColor =
                     bus.status === "Maintenance"
                       ? "text-red-500"
                       : "text-black";
+                  const isSelected = selectedBus === bus.number;
+                  const selectedBorder = isSelected
+                    ? "border-4 border-green-500"
+                    : "";
 
                   return (
-                    <div
+                    <button
                       key={bus.number}
-                      className={`container w-96 p-4 rounded-lg flex flex-row space-x-14 ${bgColor}`}
+                      onClick={() => setSelectedBus(bus.number)}
+                      className={`container w-96 p-4 rounded-lg flex flex-row space-x-14 ${bgColor} ${selectedBorder}`}
                     >
-                      {/* Icon */}
                       <FaBus
                         size={30}
                         className="ml-2 cursor-pointer text-black"
                       />
-
-                      {/* Bus number */}
                       <h1>
                         Bus{" "}
                         <span id={`bus-number-${bus.number}`}>
                           {bus.number}
                         </span>
                       </h1>
-
-                      {/* Bus status with dynamic text color */}
                       <h1 id={`status-${bus.number}`} className={textColor}>
                         {bus.status}
                       </h1>
-                    </div>
+                    </button>
                   );
                 })}
+                <div className="pagination mt-4 flex space-x-2 items-center">
+                  {/* Previous Arrow */}
+                  <button
+                    onClick={() => handlePageChange(currentPage - 1)}
+                    className={`px-3 py-1 border-2 rounded transition-colors duration-300 ${
+                      currentPage === 1
+                        ? "border-gray-300 text-gray-400 cursor-not-allowed"
+                        : "border-gray-300 text-gray-700 hover:bg-gray-100"
+                    }`}
+                    disabled={currentPage === 1}
+                  >
+                    &lt;
+                  </button>
+                  {/* Page Buttons */}
+                  <button
+                    onClick={() => handlePageChange(1)}
+                    className={`px-3 py-1 border-2 rounded transition-colors duration-300 ${
+                      currentPage === 1
+                        ? "bg-blue-500 text-white border-blue-500"
+                        : "border-gray-300 text-gray-700 hover:bg-gray-100"
+                    }`}
+                  >
+                    1
+                  </button>
+                  <button
+                    onClick={() => handlePageChange(2)}
+                    className={`px-3 py-1 border-2 rounded transition-colors duration-300 ${
+                      currentPage === 2
+                        ? "bg-blue-500 text-white border-blue-500"
+                        : "border-gray-300 text-gray-700 hover:bg-gray-100"
+                    }`}
+                  >
+                    2
+                  </button>
+                  {/* Next Arrow */}
+                  <button
+                    onClick={() => handlePageChange(currentPage + 1)}
+                    className={`px-3 py-1 border-2 rounded transition-colors duration-300 ${
+                      currentPage === totalPages
+                        ? "border-gray-300 text-gray-400 cursor-not-allowed"
+                        : "border-gray-300 text-gray-700 hover:bg-gray-100"
+                    }`}
+                    disabled={currentPage === totalPages}
+                  >
+                    &gt;
+                  </button>
+                </div>
               </div>
               <div className="arrow">
                 <FaAngleDoubleRight
@@ -148,33 +213,28 @@ const DispatchMonitoring = () => {
                   className="cursor-pointer text-violet-800"
                 />
               </div>
-              <div className="bus-information w-1/4 bg-white h-auto rounded-lg border-2 border-violet-400 ">
+              <div className="bus-information w-1/4 bg-white h-auto rounded-lg border-2 border-violet-400">
                 <div className="infos flex flex-col ml-5">
-                  <div className="header-info flex flex-row mt-4  space-x-5">
+                  <div className="header-info flex flex-row mt-4 space-x-5">
                     <FaBus
                       size={40}
-                      className="ml-2 cursor-pointer text-violet-600"
+                      className="cursor-pointer text-black"
                     />
-                    <h1 className="text-red-600 text-xl font-bold">BUS 03</h1>
+                    <h1 className="text-2xl font-bold">
+                      {selectedBus ? `Bus ${selectedBus}` : "Bus"}
+                    </h1>
                   </div>
-                  <div className="info-text mt-10">
-                    <ul className="list-disc list-inside space-y-2 mb-4 text-lg">
-                      <li>
-                        <strong>Driver:</strong> James Harden
-                      </li>
-                      <li>
-                        <strong>Conductor:</strong> Stephen Curry
-                      </li>
-                      <li>
-                        <strong>Plate number:</strong> KVJ-232-2313
-                      </li>
-                      <li>
-                        <strong>Trips:</strong> 5
-                      </li>
-                      <li>
-                        <strong>Status:</strong> In Operation
-                      </li>
-                    </ul>
+                  <hr className="border-gray-300 mt-3" />
+                  <div className="details mt-4">
+                    <p>Bus Number: {selectedBus}</p>
+                    {selectedBus && (
+                      <p>Status: {
+                        busData.find(bus => bus.number === selectedBus)?.status
+                      }</p>
+                    )}
+                    <p className="mt-2">Driver: James Harden</p>
+                    <p>Conductor: Stephen Curry</p>
+                    <p className="mt-2">Trips: {/* Add trip details here if needed */}</p>
                   </div>
                 </div>
               </div>
