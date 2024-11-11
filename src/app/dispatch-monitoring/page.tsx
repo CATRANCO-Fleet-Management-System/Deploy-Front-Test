a55c1e272029bf4300e5f731834a5b4d4948a539
Kent Bryan Asilo <asilo.kentbryanv19@gmail.com>
2024-11-11 19:13:25 +0800

    ADJUSTED SOME IN DISPATCH MONITORING

--- src/app/dispatch-monitoring/page.tsx
+++ src/app/dispatch-monitoring/page.tsx
@@ -7,10 +7,9 @@ import { FaBus, FaAngleDoubleRight } from "react-icons/fa";
 const DispatchMonitoring = () => {
   const [dropdownVisible, setDropdownVisible] = useState(false);
   const [selectedBus, setSelectedBus] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1); // Page state
+  const [currentPage, setCurrentPage] = useState(1);
   const dropdownRef = useRef(null);
 
-  // Sample bus data simulating a backend response
   const busData = [
     { number: "001", status: "Maintenance" },
     { number: "002", status: "On Road" },
@@ -29,20 +28,17 @@ const DispatchMonitoring = () => {
   const itemsPerPage = 6;
   const totalPages = Math.ceil(busData.length / itemsPerPage);
 
-  // Handle page change
   const handlePageChange = (page) => {
     if (page >= 1 && page <= totalPages) {
       setCurrentPage(page);
     }
   };
 
-  // Filter bus data for the current page
   const displayedBuses = busData.slice(
     (currentPage - 1) * itemsPerPage,
     currentPage * itemsPerPage
   );
 
-  // Close dropdown if clicking outside of it
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
@@ -55,18 +51,18 @@ const DispatchMonitoring = () => {
   }, []);
 
   return (
-    <section className="h-screen flex flex-row bg-white ">
+    <div className="min-h-screen flex bg-gray-100">
       <Sidebar />
-      <div className="w-full bg-slate-200">
+      <div className="flex-1 flex flex-col bg-slate-200 pb-10">
         <Header title="Dispatch Monitoring" />
-        <section className="right w-full overflow-y-hidden">
-          <div className="content flex flex-col h-full ml-20">
+        <section className="p-4 flex flex-col items-center">
+          <div className="w-5/6 flex flex-col h-full">
             <div className="bus-location">
               <div className="output flex flex-row space-x-2 mt-8">
-                <div className="locations w-auto bg-white h-auto rounded-lg border-2 border-violet-400">
+                <div className="locations w-full bg-white h-auto rounded-lg border-2 border-violet-400">
                   <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15784.663154977601!2d124.5866112118501!3d8.48325558794408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32ff8ca6efa76217%3A0x8a2c13aa584988a4!2sCanito-an%2C%20Misamis%20Oriental!5e0!3m2!1sen!2sph!4v1729050669858!5m2!1sen!2sph"
-                    width="1260"
+                    width="100%"
                     height="450"
                     style={{ border: 1 }}
                     allowFullScreen
@@ -76,9 +72,8 @@ const DispatchMonitoring = () => {
                 </div>
               </div>
             </div>
-            <div className="bus-info flex flex-row mt-12 space-x-8">
+            <div className="bus-info flex flex-row mt-12 space-x-4">
               <div className="col-bus space-y-4">
-                {/* Loop through displayed buses for the current page */}
                 {displayedBuses.slice(0, 3).map((bus) => {
                   const bgColor =
                     bus.status === "Maintenance"
@@ -97,7 +92,7 @@ const DispatchMonitoring = () => {
                     <button
                       key={bus.number}
                       onClick={() => setSelectedBus(bus.number)}
-                      className={`container w-96 p-4 rounded-lg flex flex-row space-x-14 ${bgColor} ${
+                      className={`container w-80 p-4 rounded-lg flex flex-row space-x-8 ${bgColor} ${
                         isSelected ? "ring-2 ring-blue-500 shadow-lg" : ""
                       } transition-all duration-200`}
                     >
@@ -109,17 +104,12 @@ const DispatchMonitoring = () => {
                       />
                       <h1
                         className={`${
-                          isSelected ? "font-bold text-blue-700" : ""
+                          isSelected ? "font-bold text-blue-700 " : ""
                         }`}
                       >
-                        Bus{" "}
-                        <span id={`bus-number-${bus.number}`}>
-                          {bus.number}
-                        </span>
+                        Bus <span>{bus.number}</span>
                       </h1>
-                      <h1 id={`status-${bus.number}`} className={textColor}>
-                        {bus.status}
-                      </h1>
+                      <h1 className={textColor}>{bus.status}</h1>
                     </button>
                   );
                 })}
@@ -138,15 +128,12 @@ const DispatchMonitoring = () => {
                       ? "text-red-900"
                       : "text-black";
                   const isSelected = selectedBus === bus.number;
-                  const selectedBorder = isSelected
-                    ? "border-4 border-green-500"
-                    : "";
 
                   return (
                     <button
                       key={bus.number}
                       onClick={() => setSelectedBus(bus.number)}
-                      className={`container w-96 p-4 rounded-lg flex flex-row space-x-14 ${bgColor} ${
+                      className={`container w-80 p-4 rounded-lg flex flex-row space-x-8 ${bgColor} ${
                         isSelected ? "ring-2 ring-blue-500 shadow-lg" : ""
                       } transition-all duration-200`}
                     >
@@ -161,19 +148,13 @@ const DispatchMonitoring = () => {
                           isSelected ? "font-bold text-blue-700" : ""
                         }`}
                       >
-                        Bus{" "}
-                        <span id={`bus-number-${bus.number}`}>
-                          {bus.number}
-                        </span>
+                        Bus <span>{bus.number}</span>
                       </h1>
-                      <h1 id={`status-${bus.number}`} className={textColor}>
-                        {bus.status}
-                      </h1>
+                      <h1 className={textColor}>{bus.status}</h1>
                     </button>
                   );
                 })}
                 <div className="pagination mt-4 flex space-x-2 items-center">
-                  {/* Previous Arrow */}
                   <button
                     onClick={() => handlePageChange(currentPage - 1)}
                     className={`px-3 py-1 border-2 rounded transition-colors duration-300 ${
@@ -185,28 +166,19 @@ const DispatchMonitoring = () => {
                   >
                     &lt;
                   </button>
-                  {/* Page Buttons */}
-                  <button
-                    onClick={() => handlePageChange(1)}
-                    className={`px-3 py-1 border-2 rounded transition-colors duration-300 ${
-                      currentPage === 1
-                        ? "bg-blue-500 text-white border-blue-500"
-                        : "border-gray-300 text-gray-700 hover:bg-gray-100"
-                    }`}
-                  >
-                    1
-                  </button>
-                  <button
-                    onClick={() => handlePageChange(2)}
-                    className={`px-3 py-1 border-2 rounded transition-colors duration-300 ${
-                      currentPage === 2
-                        ? "bg-blue-500 text-white border-blue-500"
-                        : "border-gray-300 text-gray-700 hover:bg-gray-100"
-                    }`}
-                  >
-                    2
-                  </button>
-                  {/* Next Arrow */}
+                  {[1, 2].map((page) => (
+                    <button
+                      key={page}
+                      onClick={() => handlePageChange(page)}
+                      className={`px-3 py-1 border-2 rounded transition-colors duration-300 ${
+                        currentPage === page
+                          ? "bg-blue-500 text-white border-blue-500"
+                          : "border-gray-300 text-gray-700 hover:bg-gray-100"
+                      }`}
+                    >
+                      {page}
+                    </button>
+                  ))}
                   <button
                     onClick={() => handlePageChange(currentPage + 1)}
                     className={`px-3 py-1 border-2 rounded transition-colors duration-300 ${
@@ -222,11 +194,11 @@ const DispatchMonitoring = () => {
               </div>
               <div className="arrow">
                 <FaAngleDoubleRight
-                  size={20}
+                  size={15}
                   className="cursor-pointer text-violet-800"
                 />
               </div>
-              <div className="bus-information w-1/4 bg-white h-auto rounded-lg border-2 border-violet-400">
+              <div className="bus-information w-2/6 bg-white h-auto rounded-lg border-2 border-violet-400 p-4">
                 <div className="infos flex flex-col ml-5">
                   <div className="header-info flex flex-row mt-4 space-x-5">
                     <FaBus size={40} className="cursor-pointer text-black" />
@@ -248,9 +220,6 @@ const DispatchMonitoring = () => {
                     )}
                     <p className="mt-2">Driver: James Harden</p>
                     <p>Conductor: Stephen Curry</p>
-                    <p className="mt-2">
-                      Trips: {/* Add trip details here if needed */}
-                    </p>
                   </div>
                 </div>
               </div>
@@ -258,7 +227,7 @@ const DispatchMonitoring = () => {
           </div>
         </section>
       </div>
-    </section>
+    </div>
   );
 };
 
