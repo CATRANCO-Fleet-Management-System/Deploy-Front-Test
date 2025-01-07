faa62652a491a96b0f61cf9f3c74d221c63658b0
Kent Bryan Asilo <asilo.kentbryanv19@gmail.com>
2025-01-07 18:39:38 +0800

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Set environment variables for your app
   env: {
     NEXT_PUBLIC_FLESPI_TOKEN: process.env.NEXT_PUBLIC_FLESPI_TOKEN,
   },
+
+  // Disable ESLint during the build process to avoid linting errors blocking deployments
   eslint: {
     ignoreDuringBuilds: true,
   },
