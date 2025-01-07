a1d0d1981b049e87af641b062a31587140359821
Kent Bryan Asilo <asilo.kentbryanv19@gmail.com>
2025-01-08 03:47:49 +0800

    date

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,19 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Set environment variables for your app
+  output: "export", // Only if static site export is needed
   env: {
     NEXT_PUBLIC_FLESPI_TOKEN: process.env.NEXT_PUBLIC_FLESPI_TOKEN,
   },
-
-  // Disable ESLint during the build process to avoid linting errors blocking deployments
   eslint: {
     ignoreDuringBuilds: true,
   },
-
-  // Correctly define output
-  output: "export",
-
-  // Configure image handling
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "*.com",
-        port: "", // Leave empty if no specific port is needed
-        pathname: "/**/*", // Correct wildcard pattern
+        pathname: "/**",
       },
     ],
   },
 };
-
 export default nextConfig;
